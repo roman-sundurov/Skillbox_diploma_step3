648db402eb1b86fc05bd0a0053c1c19dd33f5eac
Roman Sundurov <roman.sundurov.work@gmail.com>
2021-12-12 20:33:25 +0300

    Initial Commit

---
+++ Skillbox_diploma_step3UITests/Skillbox_diploma_step3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Skillbox_diploma_step3UITestsLaunchTests.swift
+//  Skillbox_diploma_step3UITests
+//
+//  Created by Roman on 12.12.2021.
+//
+
+import XCTest
+
+class Skillbox_diploma_step3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
