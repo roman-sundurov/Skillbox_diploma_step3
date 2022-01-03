49682537f8e369b4a0c5e731abf9dbff38493d45
Roman Sundurov <roman.sundurov.work@gmail.com>
2022-01-03 18:24:42 +0300

    Working on: create the test Image for setting up scrollView.

--- Skillbox_diploma_step3/MapVIew.swift
+++ Skillbox_diploma_step3/MapVIew.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 @IBDesignable class MapVIew: UIView {
-
+    
     override func draw(_ rect: CGRect) {
       let path = UIBezierPath(ovalIn: rect)
       UIColor.green.setFill()
--- Skillbox_diploma_step3/VCMap.swift
+++ Skillbox_diploma_step3/VCMap.swift
@@ -9,14 +9,70 @@ import UIKit
 
 class VCMap: UIViewController {
 
-    @IBOutlet var mapView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var mapViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var mapViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var mapViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var mapViewTrailingConstraint: NSLayoutConstraint!
+
+//    @IBOutlet var mapView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
     
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .darkContent
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let statusBarView = UIView(frame: UIApplication.shared.statusBarFrame)
+        statusBarView.backgroundColor = UIColor.systemGreen
+        view.addSubview(statusBarView)
+
+    }
+    
+
+    override func viewWillLayoutSubviews() {
+      super.viewWillLayoutSubviews()
+      updateMinZoomScaleForSize(view.bounds.size)
     }
 
+    
+    func updateMinZoomScaleForSize(_ size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.zoomScale = minScale
+    }
 
 }
 
+
+extension VCMap: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        updateConstraintsForSize(view.safeAreaLayoutGuide.layoutFrame.size)
+    }
+
+    func updateConstraintsForSize(_ size: CGSize) {
+        
+        let yOffset = max(0, (size.height - imageView.frame.height) / 2)
+        mapViewTopConstraint.constant = yOffset
+        mapViewBottomConstraint.constant = yOffset
+        //4
+        let xOffset = max(0, (size.width - imageView.frame.width) / 2)
+        mapViewLeadingConstraint.constant = xOffset
+        mapViewTrailingConstraint.constant = xOffset
+
+        view.layoutIfNeeded()
+    }
+
+}
